615531dd1a82d7f041787452cc78b449409c9066
nahidul-fahim <nahidulfahim.official@gmail.com>
2023-09-13 14:27:57 +0600

    created blog cards using react

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,9 +1,29 @@
 import PropTypes from 'prop-types';
 
-const Blog = () => {
+const Blog = ({blog}) => {
+    const {title, tags, cover_img, author_img, author_name, posted_date, reading_time} = blog;
     return (
         <div>
-            
+            <img src= {cover_img} alt= {`Cover image - ${title}`} className='rounded'/>
+            <div className='flex justify-between items-center'>
+                <div className='flex justify-start items-center gap-x-4'>
+                    <div>
+                        <img src= {author_img} alt="" className='w-[60px] h-[60px]'/>
+                    </div>
+                    <div>
+                        <h5 className='text-xl font-semibold'> {author_name} </h5>
+                        <h6 className='text-base text-[gray]'>{posted_date}</h6>
+                    </div>
+                </div>
+
+                <div>
+                    <p className='text-base text-[gray]'>{reading_time} min read</p>
+                </div>
+            </div>
+            <h2 className="text-3xl font-bold"> {title} </h2>
+            <p className='text-base text-gray-500'> {tags.map((tag, idx) => <span key={idx}> <a href="">{tag}</a></span> )
+
+            } </p>
         </div>
     );
 };
--- src/Components/Blogs/Blogs.jsx
+++ src/Components/Blogs/Blogs.jsx
@@ -1,17 +1,23 @@
 import { useEffect, useState } from "react";
+import Blog from "../Blog/Blog";
 
 const Blogs = () => {
     const [blogs, setBlogs] = useState([]);
 
     useEffect(() => {
         fetch('blogs.json')
-        .then(res => res.json())
-        .then(data => setBlogs(data))
+            .then(res => res.json())
+            .then(data => setBlogs(data))
     }, [])
 
     return (
         <div className="md:w-2/3 w-full">
-            <h2 className="text-4xl font-bold">Bolgs: {blogs.length} </h2>
+            {
+                blogs.map(blog => <Blog
+                    key={blog.ID}
+                    blog={blog}>
+                </Blog>)
+            }
         </div>
     );
 };
