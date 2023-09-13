723e6fd4a058fdddd8bfa5c5af5ceff492188744
nahidul-fahim <nahidulfahim.official@gmail.com>
2023-09-13 10:55:54 +0600

    fethed the fake api data

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import { useState } from 'react'
 import './App.css'
 import Header from './Components/Header/Header'
+import Blogs from './Components/Blogs/Blogs'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <Header>
 
       </Header>
+
+      <Blogs>
+
+      </Blogs>
     </>
   )
 }
