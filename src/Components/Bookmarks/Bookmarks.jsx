360ebd1799d96851d714603ba69efa5b1cb2518d
nahidul-fahim <nahidulfahim.official@gmail.com>
2023-09-14 11:10:51 +0600

    made the website live in the surge

--- src/Components/Bookmarks/Bookmarks.jsx
+++ src/Components/Bookmarks/Bookmarks.jsx
@@ -1,8 +1,6 @@
-import PropTypes from 'prop-types';
 import Bookmark from '../Bookmark/Bookmark';
 
 const Bookmarks = ({ bookmarks, readingTime }) => {
-    console.log(readingTime);
     return (
         <div className='w-full md:w-1/3'>
             <div className='p-7 mb-10 border-[2px] border-[#6047EC] text-3xl font-semibold rounded-lg bg-[#6047ec2e]'>
@@ -12,8 +10,8 @@ const Bookmarks = ({ bookmarks, readingTime }) => {
             <div className="bg-[#dadada] rounded p-8">
                 <h2 className="text-3xl font-semibold mb-10 text-center">Bookmarked Blogs: {bookmarks.length}</h2>
                 {
-                    bookmarks.map(bookmark => <Bookmark
-                        key={bookmark.ID}
+                    bookmarks.map((bookmark, idx) => <Bookmark
+                        key={idx}
                         bookmark={bookmark}>
                     </Bookmark>)
                 }
@@ -22,9 +20,5 @@ const Bookmarks = ({ bookmarks, readingTime }) => {
     );
 };
 
-Bookmarks.propTypes = {
-    bookmarks: PropTypes.object,
-    readingTime: PropTypes.number,
-};
 
 export default Bookmarks;
